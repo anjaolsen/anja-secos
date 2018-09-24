77fc82c7b8ca824607a5911a7a85fb8559a2b70d
Anja Olsen <anja.olsen95@gmail.com>
2018-09-24 17:10:38 +0200

    Add answers to more questions

--- tp0/tp.c
+++ tp0/tp.c
@@ -8,7 +8,9 @@ extern uint32_t __kernel_start__;
 extern uint32_t __kernel_end__;
 
 
-// Informations tires de https://www.gnu.org/software/grub/manual/multiboot/multiboot.html 
+// Informations trouves sur
+// https://www.gnu.org/software/grub/manual/multiboot/multiboot.html 
+
 
 // If bit 6 in the ‘flags’ word is set, then the ‘mmap_*’ fields are valid,
 // and indicate the address and length of a buffer containing a memory map 
@@ -37,6 +39,7 @@ extern uint32_t __kernel_end__;
 // The map provided is guaranteed to list all standard ram that should be available 
 // for normal use. 
 
+
 void tp()
 {
    debug("kernel mem [0x%x - 0x%x]\n", &__kernel_start__, &__kernel_end__);
@@ -47,7 +50,7 @@ void tp()
 
    start = (multiboot_memory_map_t *) mbi->mmap_addr;
    end = (multiboot_memory_map_t *) (mbi->mmap_addr + mbi->mmap_length);
-    while ((unsigned long) start < (unsigned long) end)
+   while ((unsigned long) start < (unsigned long) end)
    {   
         debug("Addr: 0x%x, Len: 0x%x, Type = 0x%x\n", 
                 (unsigned) start->addr, (unsigned) start->len, (unsigned) start->type);
@@ -55,21 +58,69 @@ void tp()
                                    + sizeof(multiboot_memory_map_t));                                  
    }
 
-    
-        //    multiboot_memory_map_t *mmap;
+//q3 - pas de segfault!
+   int * ptr = (int *) 0x003049e0;
+   *ptr = 1;
 
-        //    for (mmap = (multiboot_memory_map_t *) mbi->mmap_addr;
-        //         (unsigned long) mmap < mbi->mmap_addr + mbi->mmap_length;
-        //         mmap = (multiboot_memory_map_t *) ((unsigned long) mmap
-        //                                  + mmap->size + sizeof (mmap->size)))
-        //      debug (" size = 0x%x, base_addr = 0x%x%08x,"
-        //              " length = 0x%x%08x, type = 0x%x\n",
-        //              (unsigned) mmap->size,
-        //              (unsigned) (mmap->addr >> 32),
-        //              (unsigned) (mmap->addr & 0xffffffff),
-        //              (unsigned) (mmap->len >> 32),
-        //              (unsigned) (mmap->len & 0xffffffff),
-        //              (unsigned) mmap->type);
-         
+   debug("%d\n", *ptr);
+
+   ptr = (int *) 0x9fc00;
+   *ptr = 1;
+
+   start = (multiboot_memory_map_t *) mbi->mmap_addr;
+   end = (multiboot_memory_map_t *) (mbi->mmap_addr + mbi->mmap_length);
+  
+   while ((unsigned long) start < (unsigned long) end)
+   {   
+        debug("Addr: 0x%x, Len: 0x%x, Type = 0x%x\n", 
+                (unsigned) start->addr, (unsigned) start->len, (unsigned) start->type);
+        start = (multiboot_memory_map_t *) ((unsigned long) start
+                                   + sizeof(multiboot_memory_map_t)); 
+                              
+   }
+
+   ptr = (int *) 0xfffc0000;
+   *ptr = 1;
+   while ((unsigned long) start < (unsigned long) end)
+   {   
+        debug("Addr: 0x%x, Len: 0x%x, Type = 0x%x\n", 
+                (unsigned) start->addr, (unsigned) start->len, (unsigned) start->type);
+        start = (multiboot_memory_map_t *) ((unsigned long) start
+                                   + sizeof(multiboot_memory_map_t)); 
+                              
+   }
+
+   //pas de sagfault parce que l on n a pas encore active les segfaults
+   //il est en plein crise existensielle, juste on ne l entends pas
+
+//q4 - hors ram
+   ptr = (int *) 0x8FFFFFF;
+   debug("%d\n", *ptr);
+   *ptr = 1;
+   debug("%d\n", *ptr);
+   //ca n a pas change la valeur du contenu de ptr
 
 }
+
+
+/*
+
+    q1
+        objdump -D kernel.elf *pipe* less 
+
+*/
+
+
+/*
+
+
+Addr: 0x0, Len: 0x9fc00, Type = 0x1 - available ram (interrupt vector area)
+Addr: 0x9fc00, Len: 0x400, Type = 0x2 - reserved ram
+Addr: 0xf0000, Len: 0x10000, Type = 0x2 - reserved ram (BIOS code area)
+Addr: 0x100000, Len: 0x7ee0000, Type = 0x1 - available ram
+Addr: 0x7fe0000, Len: 0x20000, Type = 0x2 - reserved ram
+Addr: 0xfffc0000, Len: 0x40000, Type = 0x2 - reserved ram
+
+
+*/
+
