04b5ae08a72f6ae14465667539224f5d013df5cd
Anja <anja.olsen95@gmail.com>
2018-10-11 14:00:33 +0200

    Modify GDT. Enable execution without error

--- tp3/tp.c
+++ tp3/tp.c
@@ -1,142 +1,89 @@
 /* GPLv2 (c) Airbus */
+
+// commentaire : je me permets de repartir de la correction du tp1
+// Ca petmet de mieux comprendre tous les élements de la correction
+
 #include <debug.h>
-#include <info.h>
 #include <segmem.h>
 #include <string.h>
+#include <info.h>
 
 extern info_t *info;
 
-#define CSD_TYPE 0x9A
-#define DSD_TYPE 0x92
+#define c0_idx  1
+#define d0_idx  2
+#define c3_idx  3
+#define d3_idx  4
 
-extern info_t *info;
+#define c0_sel  gdt_krn_seg_sel(c0_idx)
+#define d0_sel  gdt_krn_seg_sel(d0_idx)
+#define c3_sel  gdt_usr_seg_sel(c3_idx)
+#define d3_sel  gdt_usr_seg_sel(d3_idx)
 
-//attention : limite absolue, pas relative
-void gdt_setup_descriptor(seg_desc_t * seg_p, unsigned long base, unsigned long limit, uint8_t type){//}, ,  unsigned char access, unsignes char gran){
-    // debug("Avant chgmt:  0x%lx       Adresse:  0x%x   \n", *seg_p, seg_p);
-    uint8_t * oct = (uint8_t*)seg_p;
-    
-    if ((limit > 65536) && (limit & 0xFFF) != 0xFFF) {
-        debug("Impossible lim 0x%lx\n", limit);
-    }
-    if (limit > 65536) {
-       // Adjust granularity if required
-        debug("Gran. Ancienne limite  0x%lx\n", limit);
-        limit = limit >> 12;
-        oct[6] = 0xC0;
-        debug("Gran. Nouvelle limite  0x%lx\n", limit);
 
-    } else {
-       oct[6] = 0x40;
-    }
-    seg_p->g = 1&0x1;
+seg_desc_t GDT[6];
 
-    oct[2] = base & 0xFF;
-    oct[3] =  (base >> 8) & 0xFF;
-    oct[4] =  (base >> 16) & 0xFF;
-    oct[7] =  (base >> 24) & 0xFF;
- 
-    oct[0] = limit & 0xFF;
-    oct[1] = (limit >> 8)  & 0xFF;
-    oct[6] |= (limit >> 16)  & 0xFF;
+#define gdt_flat_dsc(_dSc_,_pVl_,_tYp_)                                 \
+   ({                                                                   \
+      (_dSc_)->raw     = 0;                                             \
+      (_dSc_)->limit_1 = 0xffff;                                        \
+      (_dSc_)->limit_2 = 0xf;                                           \
+      (_dSc_)->type    = _tYp_;                                         \
+      (_dSc_)->dpl     = _pVl_;                                         \
+      (_dSc_)->d       = 1;                                             \
+      (_dSc_)->g       = 1;                                             \
+      (_dSc_)->s       = 1;                                             \
+      (_dSc_)->p       = 1;                                             \
+   })
 
-    oct[5] = type & 0xFF;
-    seg_p->l = 1&0x1;
+#define c0_dsc(_d) gdt_flat_dsc(_d,0,SEG_DESC_CODE_XR)
+#define d0_dsc(_d) gdt_flat_dsc(_d,0,SEG_DESC_DATA_RW)
+#define c3_dsc(_d) gdt_flat_dsc(_d,3,SEG_DESC_CODE_XR)
+#define d3_dsc(_d) gdt_flat_dsc(_d,3,SEG_DESC_DATA_RW)
 
-    //debug("Apres chgmt:  0x%lx       Adresse:  0x%x   \n", *seg_p, seg_p);
-    //NB:  oct is 2 bytes as for now. look into this
-    //https://wiki.osdev.org/GDT_Tutorial#How_can_we_do_that.3F 
-    //http://www.osdever.net/bkerndev/Docs/gdt.htm
-    //http://tuttlem.github.io/2014/07/11/a-gdt-primer.html 
-    // https://manybutfinite.com/post/cpu-rings-privilege-and-protection/
+void show_gdt()
+{
+   gdt_reg_t gdtr;
+   size_t    i,n;
+
+   get_gdtr(gdtr);
+   n = (gdtr.limit+1)/sizeof(seg_desc_t);
+   for(i=0 ; i<n ; i++) {
+      seg_desc_t *dsc   = &gdtr.desc[i];
+      uint32_t    base  = dsc->base_3<<24 | dsc->base_2<<16 | dsc->base_1;
+      uint32_t    limit = dsc->limit_2<<16| dsc->limit_1;
+      debug("GDT[%d] = 0x%llx | base 0x%x | limit 0x%x | type 0x%x\n",
+            i, gdtr.desc[i].raw, base, limit, dsc->type);
+   }
+   debug("--\n");
 }
 
-void display_gdt(gdt_reg_t gdt){
+void init_gdt()
+{
+   gdt_reg_t gdtr;
 
-    unsigned short gdtr_size = gdt.limit + 1;
-    
-    int nombre_de_segments = gdtr_size / sizeof(seg_desc_t);
-    debug("Numero de segments:  %d   \n", nombre_de_segments);
+   GDT[0].raw = 0ULL;
 
-    seg_desc_t * seg_ptr = (seg_desc_t *)gdt.addr;
-    for (int i = 0; i < nombre_de_segments; i++){
-        
-        uint32_t adr = ((seg_ptr->base_1)&0xFF) + (((seg_ptr->base_2)&0xF)<<16) + (((seg_ptr->base_3)&0xF)<<24); 
-        debug("Adr seg-desc:  0x%x   \n", adr);
+   c0_dsc( &GDT[c0_idx] );
+   d0_dsc( &GDT[d0_idx] );
+   c3_dsc( &GDT[c3_idx] );
+   d3_dsc( &GDT[d3_idx] );
 
-        uint32_t lim = (seg_ptr->limit_1) + ((seg_ptr->limit_2)<<16);
-        debug("Limite seg-desc:  0x%x   \n", lim);
+   gdtr.desc  = GDT;
+   gdtr.limit = sizeof(GDT) - 1;
+   set_gdtr(gdtr);
 
-        uint32_t seg_type = ((seg_ptr->type)&0xF) + ((seg_ptr->s&0x1)<< 4) + ((seg_ptr->dpl&0x3)<<5) + ((seg_ptr->p&0x1)<<7);
-        debug("Type seg-desc:  0x%x   \n", seg_type);
+   set_cs(c0_sel);
 
-        debug("Long mode (32bits)  0x%x   \n", seg_ptr->l);
-
-        debug("Granularity  0x%x   \n", seg_ptr->g);
-
-        seg_ptr++;
-        
- 
-     // uint64_t    limit_1:16;    /* bits 00-15 of the segment limit */
-     // uint64_t    base_1:16;     /* bits 00-15 of the base address */
-     // uint64_t    base_2:8;      /* bits 16-23 of the base address */
-     // uint64_t    type:4;        /* segment type */
-     // uint64_t    s:1;           /* descriptor type */
-     // uint64_t    dpl:2;         /* descriptor privilege level */
-     // uint64_t    p:1;           /* segment present flag */
-     // uint64_t    limit_2:4;     /* bits 16-19 of the segment limit */
-     // uint64_t    avl:1;         /* available for fun and profit */
-     // uint64_t    l:1;           /* longmode */
-     // uint64_t    d:1;           /* default length, depend on seg type */
-     // uint64_t    g:1;           /* granularity */
-     // uint64_t    base_3:8;      /* bits 24-31 of the base address */
-
-        debug("------------------------------------------------\n", seg_type);
-
-    }
+   set_ss(d0_sel);
+   set_ds(d0_sel);
+   set_es(d0_sel);
+   set_fs(d0_sel);
+   set_gs(d0_sel);
 }
 
 void tp()
 {
-    gdt_reg_t gdt_addr;
-    get_gdtr(gdt_addr); //l'@ de la première entrée
-    debug("Limite :  0x%x, Adr de base : 0x%x\n", gdt_addr.limit, gdt_addr.addr);
-
-    debug("==============================================\n");
-    debug("==============Configuration GDT===============\n");
-    debug("==============================================\n");
-
-    gdt_reg_t gdt_addr_new;
-    seg_desc_t gdt_tab[6];
-    if ((int )&gdt_tab[0] % 8 != 0){
-        debug("Not aligned! T0D0. current addr = 0x%x \n", gdt_tab);
-    }
-    gdt_addr_new.desc = &gdt_tab[0];
-    gdt_addr_new.limit = 4 * sizeof(seg_desc_t) - 1;
-
-    debug("Limite :  0x%x, Adr de base : 0x%lx\n", gdt_addr_new.limit, gdt_addr_new.addr);
-
-    gdt_tab[0] = (seg_desc_t) *gdt_addr.desc ;
-    gdt_setup_descriptor(&gdt_tab[1], 0x00000, 0xfff0, CSD_TYPE);
-    gdt_setup_descriptor(&gdt_tab[2], 0x00000, 0xfff0, DSD_TYPE);
-    gdt_setup_descriptor(&gdt_tab[3], 0x600000, 0x20, DSD_TYPE);
-    gdt_setup_descriptor(&gdt_tab[4], 0x700000, 0x10000, CSD_TYPE);
-    //modifier type
-    gdt_setup_descriptor(&gdt_tab[5], 0x800000, 0x10000, DSD_TYPE);
-    //modifier type
-    gdt_tab[3].g = 0&0x1;
-    display_gdt(gdt_addr_new);
-
-    set_gdtr(gdt_addr_new); 
-    set_cs(0x08);
-    set_ds(0x10);
-    set_ss(0x10);
-    
-    set_fs(0x10);
-    set_gs(0x10);
-    //https://stackoverflow.com/questions/23978486/far-jump-in-gdt-in-bootloader
-
-    set_es(0x18);
-
-
+   show_gdt();
+   init_gdt();
 }
